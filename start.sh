69d002f9326ca58076264675dbfceb6f344d0193
Kent Chien <kent@myre.life>
2021-10-19 08:51:19 +0800

    feature: support log redirect

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
-npm run start-master &
-P1=$!
-npm run start-backup &
-P2=$!
+# stdout 跟 stderr 合併成一個 log file.
+# npm run start-master >server1-mix.log 2>&1 &
+# npm run start-backup >server2-mix.log 2>&1 &
 
-wait $P1 $P2
+# stdout 跟 stderr 分別指定不同 log file.
+npm run start-master 1>server1-out.log 2>server1-err.log &
+npm run start-backup 1>server2-out.log 2>server2-err.log &
 
 # List processes on port 3355 && 3356
 # lsof -i :3355 -i 3356
